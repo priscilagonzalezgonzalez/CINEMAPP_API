9c86131cef47203c6b961d935df6dc47e60a0398
Priscila González <psarahi.gonzalez@alumnos.udg.mx>
2020-11-18 20:30:59 -0600

    Agregar a la ruta peliculas la funcion modificar_pelicula y eliminar_pelicula, agregar a la ruta usuarios la funcion get_peliculas_usuario

--- conexion.py
+++ conexion.py
@@ -112,3 +112,43 @@ def get_pelicula(id):
         pelicula['compartido'] = row[9]
 
     return pelicula
+
+def modificar_pelicula(id, columna, valor):
+    update = f"UPDATE pelicula SET {columna} = %s WHERE id = %s"
+    cursor.execute(update, (valor, id))
+    bd.commit()
+
+    if cursor.rowcount:
+        return True
+    else:
+        return False
+
+def eliminar_pelicula(id):
+    eliminar = "DELETE FROM pelicula WHERE id = %s"
+    cursor.execute(eliminar, (id,))
+    bd.commit()
+
+    if cursor.rowcount:
+        return True
+    else:
+        return False
+
+def get_peliculas_usuario(id):
+    query = "SELECT * FROM pelicula WHERE usuarioId = %s"
+    cursor.execute(query, (id,))
+    peliculas = []
+    for row in cursor.fetchall():
+        pelicula = {
+            'id': row[0],
+            'titulo': row[1],
+            'fecha_visto': row[2],
+            'imagen': row[3],
+            'director': row[4],
+            'anio': row[5],
+            'valoracion': row[6],
+            'favorito': row[7],
+            'resenia': row[8],
+            'compartido': row[9]
+        }
+        peliculas.append(pelicula)
+    return peliculas
